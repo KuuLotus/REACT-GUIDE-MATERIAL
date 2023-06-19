a90641a73e1372bd2598c6a6366ba93dae1aa903
KuuLotus <kuulotusy@gmail.com>
2023-06-19 12:15:24 +0900

    [finish] 12_hooks_p2

--- 12_hooks_p2/src/030_useEffect_cleanup/start/Example.js
+++ 12_hooks_p2/src/030_useEffect_cleanup/start/Example.js
@@ -1,18 +1,35 @@
 import { useEffect, useState } from "react";
 
 const Example = () => {
+  const [isDisp, setIsDisp] = useState(true);
+
+  return (
+    <>
+      {isDisp && <Timer />}
+      <button onClick={() => setIsDisp((prev) => !prev)}>toggle</button>
+    </>
+  );
+};
+const Timer = () => {
   const [time, setTime] = useState(0);
 
   useEffect(() => {
-    console.log('useEffect is called');
+    // console.log("init is called");
     window.setInterval(() => {
-      setTime(prev => prev + 1);
+      setTime((prev) => prev + 1);
     }, 1000);
-  }, [])
-  
+    return () => {
+      // console.log("end");
+    };
+  }, []);
+
   useEffect(() => {
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key-end', time)
+    // console.log("updated");
+    document.title = "counter:" + time;
+    window.localStorage.setItem("time-key-end", time);
+    return () => {
+      // console.log("updated end");
+    };
   }, [time]);
 
   return (
@@ -20,7 +37,7 @@ const Example = () => {
       <time>{time}</time>
       <span>秒経過</span>
     </h3>
-    );
+  );
 };
 
 export default Example;
--- 12_hooks_p2/src/070_customHook/end/useTimer.js
+++ 12_hooks_p2/src/070_customHook/end/useTimer.js
@@ -8,55 +8,55 @@ const useTimer = () => {
     // console.log('init');
     let intervalId = null;
 
-    if(isRunning) {
+    if (isRunning) {
       // console.log('timer start');
 
       intervalId = window.setInterval(() => {
         // console.log('interval running');
-        setTime(prev => prev + 1);
+        setTime((prev) => prev + 1);
       }, 1000);
     }
-    
+
     return () => {
-      window.clearInterval(intervalId)
+      window.clearInterval(intervalId);
       // console.log('end');
-    }
-  }, [isRunning])
-  
+    };
+  }, [isRunning]);
+
   useEffect(() => {
     // console.log('updated');
-    
-    document.title = 'counter:' + time;
-    window.localStorage.setItem('time-key-end', time);
+
+    document.title = "counter:" + time;
+    window.localStorage.setItem("time-key-end", time);
 
     return () => {
       // debugger
       // console.log('updated end');
-    }
+    };
   }, [time]);
 
   useLayoutEffect(() => {
-    const _time = parseInt(window.localStorage.getItem('time-key-end'));
-    if(!isNaN(_time)) {
+    const _time = parseInt(window.localStorage.getItem("time-key-end"));
+    if (!isNaN(_time)) {
       setTime(_time);
     }
-  }, [])
+  }, []);
 
   const toggle = () => {
-    setIsRunning(prev => !prev);
-  }
+    setIsRunning((prev) => !prev);
+  };
 
   const reset = () => {
     setTime(0);
     setIsRunning(false);
-  }
-  
+  };
+
   return {
     time,
     isRunning,
     toggle,
-    reset
-  }
+    reset,
+  };
 };
 
 export default useTimer;
